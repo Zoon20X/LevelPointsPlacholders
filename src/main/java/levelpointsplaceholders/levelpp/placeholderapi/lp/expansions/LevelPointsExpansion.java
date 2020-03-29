2a1825ffca88474115f4163e796d508da6faa5bc
Zoon20X <47308184+Zoon20X@users.noreply.github.com>
2020-03-29 21:56:15 +1000

    Added Custom Top Placeholders, will require 1.2.0.5 to utilize them effectively

--- src/main/java/levelpointsplaceholders/levelpp/placeholderapi/lp/expansions/LevelPointsExpansion.java
+++ src/main/java/levelpointsplaceholders/levelpp/placeholderapi/lp/expansions/LevelPointsExpansion.java
@@ -7,15 +7,19 @@ import levelpoints.utils.utils.API;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemorySection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LevelPointsExpansion extends PlaceholderExpansion {
 
-
+    private int posTop = 0;
     public int prestige;
     private LevelPoints plugin;
 
@@ -100,6 +104,51 @@ public class LevelPointsExpansion extends PlaceholderExpansion {
         }
         if (identifier.equals("player_level")) {
             return String.valueOf(playerLevels);
+        }
+        if(identifier.contains("Top")){
+
+            ArrayList<String> name = new ArrayList<>();
+            ArrayList<Integer> level = new ArrayList<>();
+            FileConfiguration con = plugin.uc.getTopListConfig();
+            ConfigurationSection cf = con.getConfigurationSection("");
+            cf.getValues(false)
+                    .entrySet()
+                    .stream()
+                    .sorted((a1, a2) -> {
+                        int points1 = ((MemorySection) a1.getValue()).getInt("Level");
+                        int points2 = ((MemorySection) a2.getValue()).getInt("Level");
+                        return points2 - points1;
+                    }).limit(10).forEach(f -> {
+                        posTop += 1;
+
+                        int points = ((MemorySection) f.getValue()).getInt("Level");
+                        String playername = ((MemorySection) f.getValue()).getString("Name");
+
+                        name.add(playername);
+                        level.add(points);
+
+
+                    });
+
+            if(identifier.contains("Top_Name")){
+
+                String value = identifier.replace("Top_Name_", "");
+                if(name.size() < Integer.parseInt(value)){
+                    return "";
+                }else {
+                    return name.get(Integer.parseInt(value) - 1);
+                }
+            }
+            if(identifier.contains("Top_Level")) {
+                String value = identifier.replace("Top_Level_", "");
+                if (level.size() < Integer.parseInt(value)) {
+                    return "";
+                } else {
+                    return String.valueOf(level.get(Integer.parseInt(value) - 1));
+                }
+            }
+
+
         }
         if (identifier.equals("exp_amount")) {
             return String.valueOf(expamount);
